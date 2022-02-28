b092c2ff0a29e5aa3f74da3990e389a32f4d4576
sumanaashok <sumanaashok4392@gmail.com>
2022-02-27 19:37:09 -0500

    initial commit

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+"""A simple flask web app"""
+from flask import Flask, render_template
+
+
+def create_app():
+    """Create and configure an instance of the Flask application."""
+    app = Flask(__name__)
+    app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+    @app.route("/")
+    def index():
+        return render_template('index.html')
+
+    @app.route("/about")
+    def about():
+        return render_template('about.html')
+    return app
