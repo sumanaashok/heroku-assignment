d480fa74a93436d99a9903ffae608eeb34df0d45
sumanaashok <sumanaashok4392@gmail.com>
2022-02-27 20:00:42 -0500

    test updated

--- tests/homepage_test.py
+++ tests/homepage_test.py
@@ -4,4 +4,4 @@
 def test_request_example(client):
     """This makes the index page"""
     response = client.get("/")
-    assert b"Hello, Class!" in response.data
+    assert b"Some quick example text to build on the card title and make up the bulk of the card's content." in response.data
